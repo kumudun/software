79613ac96920f1df14620a2a55a1410d8ce801d3
kumudun <kumudun@metropolia.fi>
2024-08-25 23:04:17 +0300

    python exersice 1

---
+++ hello.py
@@ -0,0 +1,11 @@
+import random
+import math
+# Module 1
+# Exercise 1
+name =input("What is your name? :")
+print(f"Hello {name}!")
+
+# Module 2
+# Exercise 1
+radius =float(input("what is your radius of the circle? "))
+print(f"Area of the circle is {math.pi*radius**2:2f}")
